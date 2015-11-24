ecdc89a7b12e03457852cd020e9084774428b528
Alex Pop <al3xpop@gmail.com>
2015-11-24 08:25:11 +0000

    added hardening recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'contact@example.com'
 license 'All rights reserved'
 description 'Installs/Configures myiis-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.5.0'
+version '0.5.1'
 
 depends 'git'
 depends 'msdeploy'
--- recipes/harden.rb
+++ recipes/harden.rb
@@ -5,14 +5,6 @@
 # Copyright 2015, Great Websites Inc
 #
 
-# rule 'windows-audit-103'
-# title 'Configure System Event Log (System)'
-registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\System' do
-  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
-  recursive true
-  action :create
-end
-
 # rule 'windows-base-101'
 # title 'Safe DLL Search Mode is Enabled'
 registry_key 'HKLM\System\CurrentControlSet\Control\Session Manager' do
@@ -29,6 +21,30 @@ registry_key 'HKLM\System\CurrentControlSet\Services\LanManServer\Parameters' do
   action :create
 end
 
+# rule 'windows-base-201'
+# title 'Strong Windows NTLMv2 Authentication Enabled; Weak LM Disabled'
+registry_key 'HKLM\System\CurrentControlSet\Control\Lsa' do
+  values [{ name: 'LmCompatibilityLevel', type: :dword, data: 4 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-base-202' do
+# title 'Enable Strong Encryption for Windows Network Sessions on Clients'
+registry_key 'HKLM\System\CurrentControlSet\Control\Lsa\MSV1_0' do
+  values [{ name: 'NTLMMinClientSec', type: :dword, data: 537_395_200 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-base-203' do
+# title 'Enable Strong Encryption for Windows Network Sessions on Servers'
+registry_key 'HKLM\System\CurrentControlSet\Control\Lsa\MSV1_0' do
+  values [{ name: 'NTLMMinServerSec', type: :dword, data: 537_395_200 }]
+  recursive true
+  action :create
+end
+
 # rule 'windows-ie-101'
 # title 'IE 64-bit tab'
 registry_key 'HKLM\Software\Policies\Microsoft\Internet Explorer\Main' do
@@ -37,10 +53,58 @@ registry_key 'HKLM\Software\Policies\Microsoft\Internet Explorer\Main' do
   action :create
 end
 
+# rule 'windows-ie-102' do
+# title 'Run antimalware programs against ActiveX controls'
+registry_key 'HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3' do
+  values [{ name: '270C', type: :dword, data: 0 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-rdp-100' do
+# title 'Windows Remote Desktop Configured to Always Prompt for Password'
+registry_key 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' do
+  values [{ name: 'fPromptForPassword', type: :dword, data: 1 }]
+  recursive true
+  action :create
+end
+
 # rule 'windows-rdp-101'
 # title 'Strong Encryption for Windows Remote Desktop Required'
 registry_key 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' do
   values [{ name: 'MinEncryptionLevel', type: :dword, data: 3 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-audit-100'
+# title 'Configure System Event Log (Application)'
+registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\Application' do
+  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-audit-101'
+# title 'Configure System Event Log (Security)'
+registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\Security' do
+  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-audit-102'
+# title 'Configure System Event Log (Setup)'
+registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\Setup' do
+  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
+  recursive true
+  action :create
+end
+
+# rule 'windows-audit-103'
+# title 'Configure System Event Log (System)'
+registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\System' do
+  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
   recursive true
   action :create
 end
